eccbe0f2a72aff6a686e6408a1754e9320402604
Roman Humagain <127822516+romanhumagain@users.noreply.github.com>
2023-05-25 09:27:12 -0700

    updated code

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -8,13 +8,13 @@ def index(request):
    
 def analyze(request):
     # get the text
-    djtext =(request.GET.get('text', 'default'))
-    removepunc = request.GET.get('removepunc', 'off')
-    full_caps = request.GET.get("captialize", "off")
-    newline = request.GET.get('newline', "off")
-    spaceremover = request.GET.get("spaceremover", "off")
-    charactercount = request.GET.get("charactercount", "off")
-    wordcount = request.GET.get("wordcount", "off")
+    djtext =(request.POST.get('text', 'default'))
+    removepunc = request.POST.get('removepunc', 'off')
+    full_caps = request.POST.get("captialize", "off")
+    newline = request.POST.get('newline', "off")
+    spaceremover = request.POST.get("spaceremover", "off")
+    charactercount = request.POST.get("charactercount", "off")
+    wordcount = request.POST.get("wordcount", "off")
 
     puncts ='''!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~'''
     if removepunc == 'on':
@@ -28,7 +28,7 @@ def analyze(request):
     'purpose':'Remove Punctuation','analyzed_text' : analyzed}
         return render(request, 'analyze.html',parameters)
 
-    if (full_caps == "on"):
+    elif (full_caps == "on"):
         analyzed = ""
 
         for char in djtext:
@@ -37,12 +37,23 @@ def analyze(request):
         parameters = {'purpose':'Capitalize','analyzed_text' : analyzed}
         return render(request, 'analyze.html',parameters)
     
-    if(newline == "on"):
-        analyzed = djtext.replace("\n", " ")
-        parameters = {'purpose':'New Line Remover','analyzed_text' : analyzed }
+    elif (newline == "on"):
+        analyzed = ""
+        skip_space = False
+        for char in djtext:
+            if char != "\n" and char != "\r":
+                if char.isspace():
+                    if not skip_space:
+                        analyzed += char
+                    skip_space = True
+                else:
+                    analyzed += char
+                    skip_space = False
+        print("pre", analyzed)
+        parameters = {'purpose': 'New Line Remover', 'analyzed_text': analyzed}
         return render(request, 'analyze.html', parameters)
-
-    if(spaceremover == "on"):
+    
+    elif (spaceremover == "on"):
         analyzed = ""
         for index, char in enumerate(djtext):
             if not (djtext[index] == " " and djtext[index + 1] == " "):
@@ -50,7 +61,7 @@ def analyze(request):
         parameters = {'purpose':'Space Remover Between Words','analyzed_text' : analyzed }
         return render(request, 'analyze.html',parameters)
     
-    if (charactercount == "on"):
+    elif (charactercount == "on"):
         analyzed_count = 0
 
         for char in djtext:
@@ -59,15 +70,15 @@ def analyze(request):
         parameters = {'purpose':'Character Counter','analyzed_text' : "The Number Of Character is " +str(analyzed_count) }
         return render(request , "analyze.html", parameters)
 
-    if(wordcount == "on"):
+    elif (wordcount == "on"):
         word_count = 0
         listed_word =djtext.split()
         for i in listed_word:
             word_count +=1
         parameters = {'purpose':'Character Counter','analyzed_text' : "The Number Of Words is " +str(word_count) }
         return render(request , "analyze.html", parameters)
-    # else:
-    #     return HttpResponse("ERROR 404")
+    else:
+        return HttpResponse("ERROR 404")
     
     
     
